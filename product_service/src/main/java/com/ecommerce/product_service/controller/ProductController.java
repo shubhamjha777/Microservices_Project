5f59a21fee69e344e56d4f277f1dca88d867fc58
Shubham <shubhamkjha27@gmail.com>
2023-01-23 10:56:10 +0530

    Initial Changes

---
+++ product_service/src/main/java/com/ecommerce/product_service/controller/ProductController.java
@@ -0,0 +1,30 @@
+package com.ecommerce.product_service.controller;
+
+import com.ecommerce.product_service.dto.ProductRequestDTO;
+import com.ecommerce.product_service.dto.ProductResponseDTO;
+import com.ecommerce.product_service.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/product")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody ProductRequestDTO productRequestDTO){
+          productService.createProduct(productRequestDTO);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponseDTO> getAllProducts(){
+        return productService.getAllProducts();
+    }
+}
